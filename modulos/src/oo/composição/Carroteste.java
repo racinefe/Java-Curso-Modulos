4d18e43efbde25e176b97728ccdc10689d7b663d
@racinefell <102471647+racinefe@users.noreply.github.com>
2023-12-05 11:20:22 -0300

    modulo O.O Composição

---
+++ modulos/src/oo/composição/Carroteste.java
@@ -0,0 +1,35 @@
+package oo.composição;
+
+public class Carroteste {
+
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro();
+		System.out.println(c1.estaLigado());
+		
+		c1.ligar();
+		System.out.println(c1.estaLigado());
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		
+		//faltou Encapsulamento!!!
+		//c1.motor.fatorInjecao = -3000;
+						
+		System.out.println(c1.motor.giros());
+		
+
+	}
+
+}
