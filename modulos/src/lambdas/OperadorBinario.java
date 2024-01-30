15f592d38666fdad018aa764c4337ca564e3dff8
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-01-30 08:14:03 -0300

    Binary Operator e BiFunction

---
+++ modulos/src/lambdas/OperadorBinario.java
@@ -0,0 +1,22 @@
+package lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
+public class OperadorBinario {
+
+	public static void main(String[] args) {
+		
+		BinaryOperator<Double> media = (Double n1,Double n2) -> (n1 + n2) / 2;
+		System.out.println(media.apply(9.8, 5.7));
+		
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+			double notaFinal = (n1 + n2) / 2;
+			return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+		};
+		
+		System.out.println(resultado.apply(9.7, 4.1));
+
+	}
+
+}
