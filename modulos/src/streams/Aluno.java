b0759e4afaa1eb7c93f813c3a096f515f4473971
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-05-03 09:52:40 -0300

    Filter #1 Filter #2

---
+++ modulos/src/streams/Aluno.java
@@ -0,0 +1,13 @@
+package streams;
+
+public class Aluno {
+	
+	final String nome;
+	final Double nota;
+	
+	public Aluno(String nome, Double nota) {
+		this.nome = nome;
+		this.nota = nota;
+	}
+	
+}
